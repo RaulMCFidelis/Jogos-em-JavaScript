b82c7c06c6adb6ae36646a54d38d8d8cee1b122f
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-05 09:43:54 -0300

    If Else

--- Introducao/Aula4/script.js
+++ Introducao/Aula4/script.js
@@ -14,3 +14,16 @@ else if(vel>100){
 else if(vel == 0){
     alert("parado")
 }
+
+/* let nota = 61;
+if(nota>=60){
+    alert("aprovado");
+}
+else if(nota<60 && nota>40){
+    alert("pode fazer a prova de recuperaçao");
+}
+else{
+    alert("reprovado");
+}
+*/
+    
