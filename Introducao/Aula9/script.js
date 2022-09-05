bc8133d51a0e43b1d78b552845e728e416e9e4f5
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-05 12:01:57 -0300

    While

---
+++ Introducao/Aula9/script.js
@@ -0,0 +1,24 @@
+// While
+// Inicializando a variavel de soma
+let total = 0;
+
+// Inicializando a variavel de limite
+let j = 1;
+
+// Enquanto j for menor ou igual a 10, faça:
+while(j<=10){
+    total = total + 1
+    console.log("Interacao", total)
+    j++;
+}
+
+/* let vidas = 100;
+let colidindo = true;
+
+while(colidindo){
+    vida -= 1;
+    console.log(vidas);
+    if(vidas == 0){
+        break;
+    }
+} */
