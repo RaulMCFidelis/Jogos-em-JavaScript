e2c4cb5e29d1e5fa43b4a19b68fdb2b0f1c7c619
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-05 09:02:17 -0300

    Operadores Condicionais

---
+++ Introducao/Aula3/script.js
@@ -0,0 +1,26 @@
+alert("To funcionando");
+
+// Operadores Condicionais
+
+// == -> Operador de igualdade
+// != -> diferença
+// > -> maior que
+// >= -> naior ou ingual
+// < -> menor que
+// <= -> menor ou igual
+
+let a = 100;
+let b = 200;
+let c = a < b;
+
+//alert(c);
+
+let d = (a == b);
+//alert(d);
+
+let e = a != b;
+alert(e);
+
+// Crie as variáveis a(valor = 1), b(valor = 2) e exiba no console.log i resultado de:
+// a>=b; a<=b; a!=b; a == b. 
+
