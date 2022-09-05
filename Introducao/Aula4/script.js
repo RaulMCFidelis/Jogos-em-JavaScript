427f7a8fe64fd13393c482ff9845bd506b5adc9e
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-05 09:38:31 -0300

    If e Else

---
+++ Introducao/Aula4/script.js
@@ -0,0 +1,16 @@
+//alert(Ok!);
+
+// If e Else
+
+let vel;
+vel = 0;
+
+if(vel>0 && vel<100){
+    alert("andando");
+}
+else if(vel>100){
+    alert("correndo");
+}
+else if(vel == 0){
+    alert("parado")
+}
