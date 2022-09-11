4f1745511a38190e6ed0d181d7866b7fe8efc0db
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-10 21:19:08 -0300

    Configurar-a-cobra

--- Snake/script.js
+++ Snake/script.js
@@ -12,4 +12,27 @@ window.onload = function(){
     velY = 0;
     grid = 20;
     tam = 3;
+
+    //Chamada da função jogo a cada 100 milisegundos
+    setInterval(jogo, 100)
+
+}
+
+function jogo(){
+    //Configuração da tela
+    //Cores htmlcolorcodes.com
+    ctx.fillStyle = "#2980B9"
+
+    //Distancia da borada h, distancia da borda vertical, largura, altura
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+
+    //Posicionando a cobra
+    snake.push({x: positionX, y: positionY})
+    console.log(snake[0])
+
+    //Configurar a cobra
+    ctx.fillStyle = "#00f102";
+    for(let i=0; i<snake.length; i++){
+        ctx.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1)
+    }
 }
