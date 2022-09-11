8fcf480572affb29662387dbd3695bf433b07a21
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-11 19:35:15 -0300

    Criando-a-tela-inicial-do-jogo

--- Codi-Memory/app.js
+++ Codi-Memory/app.js
@@ -52,4 +52,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
        
     ]
+
+    const grid = document.querySelector('.grid')
+
+    //Criando a tela
+    function createBoard(){
+        for(let i=0; i<cardArray.length;i++){
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/card.png')
+            card.setAttribute('data-id',i)
+            //card.addEventListener('click',flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    createBoard()
+
+
 })
