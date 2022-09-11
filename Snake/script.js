0082a4fb1dae687385c065c4faab1273119d2d17
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-11 09:14:09 -0300

    Controles-e-Espelhamento

--- Snake/script.js
+++ Snake/script.js
@@ -16,6 +16,32 @@ window.onload = function(){
     //Chamada da função jogo a cada 100 milisegundos
     setInterval(jogo, 100)
 
+    //Controles
+    document.addEventListener("keydown", function(e){
+        switch(e.keyCode){
+            //Seta direita = 39
+            case 39:
+                velX = 1;
+                velY = 0;
+                break;
+            //seta esquerda = 37
+            case 37:
+                velX = -1;
+                velY = 0;
+                break;
+            //seta cima = 38
+            case 38:
+                velX = 0;
+                velY = -1;
+                break;
+            //seta baixo = 40
+            case 40:
+                velX = 0;
+                velY = 1;
+                break;
+        }
+    })
+
 }
 
 function jogo(){
@@ -26,13 +52,35 @@ function jogo(){
     //Distancia da borada h, distancia da borda vertical, largura, altura
     ctx.fillRect(0,0, canvas.width, canvas.height)
 
+    //Deskicamento da cobra
+    positionX += velX;
+    positionY += velY;
+
+    //Espelhamento
+    if(positionX < 0){
+        positionX = grid;
+    }
+    if(positionX > grid){
+        positionX = 0;
+    }
+    if(positionY < 0){
+        positionY = grid;
+    }
+    if(positionY > grid){
+        positionY = 0;
+    }
+
     //Posicionando a cobra
     snake.push({x: positionX, y: positionY})
-    console.log(snake[0])
 
     //Configurar a cobra
     ctx.fillStyle = "#00f102";
     for(let i=0; i<snake.length; i++){
         ctx.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1)
+    }
+
+    //Apagando
+    while(snake.length > tam){
+        snake.shift();
     }
 }
