bd844d20d2c8fc91f91bee789eae414503e4e08a
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2022-09-11 17:28:11 -0300

    bird-voando

--- Flappy-Bird/script.js
+++ Flappy-Bird/script.js
@@ -20,9 +20,41 @@ var bX = 33;
 var bY = 200;
 var gravity = 1.4;
 var score = 0;
+var cano = [];
+
+cano[0] = {
+    x : canvas.clientWidth,
+    y : 0
+}
 
 //Carregando sons
 var fly = new Audio();
 fly.src = "sounds/fly.mp3"
 var scor = new Audio();
 scor.src = "sounds/score.mp3"
+
+    //Captura de tecla
+    document.addEventListener("keydown", voa);
+
+    //voando
+    function voa(){
+            bY = bY - 26;
+            fly.play();
+    }
+
+function jogo(){
+    //Fundo do Jogo
+    ctx.drawImage(bg,0,0);
+    //drawImage(imgem, X, Y)
+
+    // desenhando o chão
+    ctx.drawImage(chao,0,canvas.height - chao.height);
+
+    //Passaro
+    ctx.drawImage(bird,bX,bY);
+    bY += gravity;
+
+    requestAnimationFrame(jogo);
+}
+
+jogo();
