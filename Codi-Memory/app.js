c6e6485cb4702d987aba31760200517555df22e7
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-07-08 01:19:55 -0300

    Projeto Final do Codi Memory

--- Codi-Memory/app.js
+++ Codi-Memory/app.js
@@ -1,90 +1,129 @@
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    //Carregamento dos cards
-    const cardArray = [
-    {
-        name: 'ganhou',
-        img: 'images/ganhou.png'
-    },
-    {
-        name: 'ganhou',
-        img: 'images/ganhou.png'
-    },
-    {
-        name: 'direita',
-        img: 'images/direita.png'
-    },
-    {
-        name: 'direita',
-        img: 'images/direita.png'
-    },
-    {
-        name: 'tras',
-        img: 'images/tras.png'
-    },
-    {
-        name: 'tras',
-        img: 'images/tras.png'
-    },
-    {
-        name: 'correndo',
-        img: 'images/correndo.png'
-    },
-    {
-        name: 'correndo',
-        img: 'images/correndo.png'
-    },
-    {
-        name: 'pulo',
-        img: 'images/pulo.png'
-    }, 
-    {
-        name: 'pulo',
-        img: 'images/pulo.png'
-    },
-    {
-        name: 'esquerda',
-        img: 'images/esquerda.png'
-    },
-    {
-        name: 'esquerda',
-        img: 'images/esquerda.png'
-    }
-       
-    ]
-    //Misturar os cards
-    cardArray.sort(() => 0.5 - Math.random())
+	// Carregamento dos cards
+	const cardArray = [
+	{
+		name: 'ganhou',
+		img: 'images/ganhou.png'
+	},
+	{
+		name: 'ganhou',
+		img: 'images/ganhou.png'
+	},
+	{
+		name: 'direita',
+		img: 'images/direita.png'
+	},
+	{
+		name: 'direita',
+		img: 'images/direita.png'
+	},
+	{
+		name: 'tras',
+		img: 'images/tras.png'
+	},
+	{
+		name: 'tras',
+		img: 'images/tras.png'
+	},
+	{
+		name: 'correndo',
+		img: 'images/correndo.png'
+	},
+	{
+		name: 'correndo',
+		img: 'images/correndo.png'
+	},
+	{
+		name: 'pulo',
+		img: 'images/pulo.png'
+	},
+	{
+		name: 'pulo',
+		img: 'images/pulo.png'
+	},
+	{
+		name: 'esquerda',
+		img: 'images/esquerda.png'
+	},
+	{
+		name: 'esquerda',
+		img: 'images/esquerda.png'
+	}
 
-    const grid = document.querySelector('.grid')
 
-    const resultDisplay = document.querySelector('#result')
-    var cardsChosen = []
-    var cardsChosenID = []
-    var pares = []
+	]
 
-    //Criando a tela
-    function createBoard(){
-        for(let i=0; i<cardArray.length;i++){
-            var card = document.createElement('img')
-            card.setAttribute('src', 'images/card.png')
-            card.setAttribute('data-id',i)
-            card.addEventListener('click',flipCard)
-            grid.appendChild(card)
-        }
-    }
+	cardArray.sort(() => 0.5 - Math.random())
 
-    // Virando cards
-    function flipCard(){
-        var cardId = this.getAttribute('data-id')
-        cardsChosen.push(cardArray[cardId].name)
-        cardsChosenID.push(cardId)
-        this.setAttribute('src', cardArray[cardId].img)
-        if(cardsChosen.length == 2){
-            setTimeout(checkForMatch, 500)
-        }
-    }
+	const grid = document.querySelector('.grid')
 
-    createBoard()
+	const resultDisplay = document.querySelector('#result')
+	var cardsChosen = []
+	var cardsChosenId = []
+	var pares = []
 
+	// Criando a tela
+	function createBoard(){
+		for(let i=0; i<cardArray.length;i++){
+			var card = document.createElement('img')
+			card.setAttribute('src', 'images/card.png')
+			card.setAttribute('data-id',i)
+			card.addEventListener('click',flipCard)
+			grid.appendChild(card)
+		}
+	}
+
+	// Conferindo pares
+	function checkforMatch(){
+		var cards = document.querySelectorAll('img')
+		const optionOneId = cardsChosenId[0]
+		const optionTwoId = cardsChosenId[1]
+
+		// Clicar duas vezes no mesmo card
+		if(optionOneId == optionTwoId) {
+			cards[optionOneId].setAttribute('src','images/card.png')
+			cards[optionTwoId].setAttribute('src','images/card.png')
+			alert("Você clicou na mesma imagem")
+		}
+
+		// Formando um par
+		else if(cardsChosen[0] == cardsChosen[1]){
+			alert("Você conseguiu um par!")
+			cards[optionOneId].setAttribute('src', 'images/white.png')
+			cards[optionTwoId].setAttribute('src', 'images/white.png')
+			cards[optionOneId].removeEventListener('click',flipCard)
+			cards[optionTwoId].removeEventListener('click',flipCard)
+			pares.push(cardsChosen)
+		} // Não formou um par
+		else {
+			cards[optionOneId].setAttribute('src','images/card.png')
+			cards[optionTwoId].setAttribute('src','images/card.png')
+			alert('Ops! Jogue novamente :)')
+		}
+		cardsChosen = []
+		cardsChosenId = []
+		resultDisplay.textContent = pares.length
+
+		if( pares.length == cardArray.length/2){
+			resultDisplay.textContent = 'Parabéns! Você encontrou todos os pares!'
+		}
+
+	}
+
+	// Virando cards
+	function flipCard() {
+		var cardId = this.getAttribute('data-id')
+		cardsChosen.push(cardArray[cardId].name)
+		cardsChosenId.push(cardId)
+		this.setAttribute('src', cardArray[cardId].img)
+		if(cardsChosen.length == 2){
+			setTimeout(checkforMatch, 500)
+		}
+	}
+
+	createBoard()
 
 })
+
